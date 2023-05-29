4a8bf026413d43e0c95778b6b064ed13c3a36100
mdsrad <mdsrad@gmail.com>
2023-05-29 20:58:18 +0200

    ultimo de guards

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { CanMatch, Route, UrlSegment, UrlTree, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanMatch, CanActivate{
+
+  constructor(
+    private authService: AuthService,
+    private router: Router) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+
+    return this.authService.checkAutentication()
+      .pipe(
+        // tap( isAuthenticated => console.log('Authenticated', isAuthenticated)),
+        tap( isAuthenticated => {
+          if( !isAuthenticated ) {
+            this.router.navigate(['./auth/login'])
+          }
+        }),
+      )
+  }
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    return this.checkAuthStatus();
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    return this.checkAuthStatus();
+  }
+
+
+
+}
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, ObservableNotification, tap } from 'rxjs';
+import { Observable, of, tap, map, catchError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -23,5 +23,24 @@ export class AuthService {
       tap( user => this.user = user ),
       tap( user => localStorage.setItem('token', user.id.toString() )),
     )
+  }
+
+  checkAutentication(): Observable<boolean> {
+
+    if ( !localStorage.getItem('token') )  return of(false);
+
+    const token = localStorage.getItem( 'token' );
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user),
+        catchError( err => of(false) )
+      )
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
 }
